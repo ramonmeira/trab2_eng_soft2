dd62f0b39aab08a2e84a68491941e95c56e7bb99
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-04-27 17:14:56 -0300

    Inserindo Cadastro de produto

---
+++ src/view/CadastroProduto.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+import view.models.SingletonJInternalFrame;
+
+public class CadastroProduto extends SingletonJInternalFrame {
+
+	public CadastroProduto() {
+		super();
+		
+//		setBounds(100, 100, 450, 300);
+
+	}
+
+}
