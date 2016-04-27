07e12e766599182f7c9b7618116b0f83b0ad7491
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-04-27 17:30:17 -0300

    Alterando master

--- src/view/CadastroProduto.java
+++ src/view/CadastroProduto.java
@@ -1,8 +1,11 @@
 package view;
 
 import java.awt.EventQueue;
+import java.beans.PropertyVetoException;
 
+import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 
 import view.models.SingletonJInternalFrame;
 
@@ -10,8 +13,17 @@ public class CadastroProduto extends SingletonJInternalFrame {
 
 	public CadastroProduto() {
 		super();
-		
-//		setBounds(100, 100, 450, 300);
+		try {
+			setMaximum(true);
+			
+			JButton btnCadastrar = new JButton("Cadastrar");
+			btnCadastrar.setBounds(130, 365, 89, 23);
+			getContentPane().add(btnCadastrar);
+		}
+		catch(PropertyVetoException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Erro de maximização", "Não é possível maximizar esta tela!", JOptionPane.WARNING_MESSAGE);
+		}
 
 	}
 
